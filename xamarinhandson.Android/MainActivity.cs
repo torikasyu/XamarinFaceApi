e2557fb5b87a5a4d2651aee26ff93a52786f0fe1
Hiroki TANAKA <tanaka.hiroki@gmail.com>
2018-08-09 12:40:07 +0900

    First Commit

---
+++ xamarinhandson.Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
+
+namespace xamarinhandson.Droid
+{
+    [Activity(Label = "xamarinhandson", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            //以下追加
+            CrossCurrentActivity.Current.Init(this, bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ xamarinhandson/FaceDetection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace xamarinhandson
+{
+    public class FaceDetection
+    {
+        public string Emotion { get; set; }
+        public double Smile { get; set; }
+        public string Glasses { get; set; }
+        public string Gender { get; set; }
+        public double Age { get; set; }
+        public double Beard { get; set; }
+        public double Moustache { get; set; }
+
+        public float Anger { get; set; }
+        public float Contempt { get; set; }
+        public float Disgust { get; set; }
+        public float Fear { get; set; }
+        public float Happiness { get; set; }
+        public float Neutral { get; set; }
+        public float Sadness { get; set; }
+        public float Surprise { get; set; }
+    }
+}
